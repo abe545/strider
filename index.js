cd4b46dd077d6dca137eb6a174f669767af2baed
abe545 <abe545@gmail.com>
2014-06-22 11:52:38 -0400

    Switched to bcryptjs from bcrypt-nodejs
    Because bcrypt-nodejs doesn't seem to be updated any longer.
    Also removed lb\ssh.js, as it is no longer needed.

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
+process.env.PATH = process.env.Path = process.env.path = process.env.ProgramW6432 + '\\Git\\bin;' + process.env['ProgramFiles(x86)'] + '\\Git\\bin;' + process.env.PATH;
 require('./bin/strider')('');
